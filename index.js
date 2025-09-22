eedf3f680b3d4fbd98a38f8cecfc97ec279f5fd6
Natalie <kelsikeichiu@gmail.com>
2025-09-22 14:32:11 -0400

    Initial Node.js app

--- index.js
+++ index.js
@@ -16,3 +16,37 @@ if (require.main === module) {
 
 // Export functions for testing
 module.exports = { add, subtract };
+
+/*
+// Simple arithmetic functions
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) throw new Error("Cannot divide by zero");
+  return a / b;
+}
+
+// Run sample operations if executed directly
+if (require.main === module) {
+  console.log("2 + 3 =", add(2, 3));
+  console.log("5 - 2 =", subtract(5, 2));
+
+  console.log("4 * 3 =", multiply(4, 3));
+  console.log("10 / 2 =", divide(10, 2));
+}
+
+// Export functions for testing
+module.exports = { add, subtract, multiply, divide };
+
+*/
--- test.js
+++ test.js
@@ -18,3 +18,34 @@ if (testsPassed) {
   console.error("Some tests failed!");
   process.exit(1);
 }
+
+
+/*
+
+const { add, subtract, multiply, divide } = require("./index");
+
+// Simple tests
+let testsPassed = true;
+
+try {
+    if (add(2, 3) !== 5) testsPassed = false;
+    if (subtract(5, 2) !== 3) testsPassed = false;
+
+    if (multiply(4, 3) !== 12) testsPassed = false;
+    if (divide(10, 2) !== 5) testsPassed = false;
+} catch (err) {
+  console.error("Error:", err.message);
+  testsPassed = false;
+}
+
+if (testsPassed) {
+  console.log("All tests passed!");
+  process.exit(0);
+} else {
+  console.error("Some tests failed!");
+  process.exit(1);
+}
+
+
+
+*/
